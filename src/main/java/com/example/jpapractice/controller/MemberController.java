e55971056f877559ee24a2b1faa224061b0780e1
신종현 <jakie5803@gmail.com>
2026-05-12 15:27:57 +0900

    과제 3

---
+++ src/main/java/com/example/jpapractice/controller/MemberController.java
@@ -0,0 +1,39 @@
+package com.example.jpapractice.controller;
+
+import com.example.jpapractice.dto.MemberCreateRequest;
+import com.example.jpapractice.dto.MemberUpdateRequest;
+import com.example.jpapractice.response.MemberResponse;
+import com.example.jpapractice.service.MemberService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.awt.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/api/v1/member")
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping
+    public Long join(@RequestBody @Valid MemberCreateRequest request) {
+        return memberService.join(request);
+    }
+
+    @GetMapping("/{memberId}")
+    public MemberResponse findMember(@PathVariable("memberId") Long memberId) {
+        return memberService.findMember(memberId);
+    }
+
+    @PatchMapping("/{memberId}")
+    public void updateMember(@PathVariable("memberId") Long memberId, @RequestBody @Valid MemberUpdateRequest request) {
+        memberService.updateMember(memberId, request);
+    }
+
+    @DeleteMapping("/{memberId}")
+    public void deleteMember(@PathVariable("memberId") Long memberId) {
+        memberService.deleteMember(memberId);
+    }
+}
